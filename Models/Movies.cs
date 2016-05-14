2b954ccde6e3c420ea1bbd9e605a8382d5350bd8
Pablo Arce Cascante <josepa_arce@hotmail.com>
2016-05-14 00:50:41 -0600

    start

---
+++ Models/Movies.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MVC5.Models
+{
+    public class Movies
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
+    }
+    public class MovieDBContext : DbContext
+    {
+        public DbSet<Movies> Movies { get; set; }
+    }
+}
---
+++ Models/MoviesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5.Models
+{
+    public class MoviesController : Controller
+    {
+        private MovieDBContext db = new MovieDBContext();
+
+        // GET: Movies
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.Movies.ToListAsync());
+        }
+
+        // GET: Movies/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Movies movies = await db.Movies.FindAsync(id);
+            if (movies == null)
+            {
+                return HttpNotFound();
+            }
+            return View(movies);
+        }
+
+        // GET: Movies/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Movies/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "ID,Title,ReleaseDate,Genre,Price")] Movies movies)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Movies.Add(movies);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(movies);
+        }
+
+        // GET: Movies/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Movies movies = await db.Movies.FindAsync(id);
+            if (movies == null)
+            {
+                return HttpNotFound();
+            }
+            return View(movies);
+        }
+
+        // POST: Movies/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "ID,Title,ReleaseDate,Genre,Price")] Movies movies)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(movies).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(movies);
+        }
+
+        // GET: Movies/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Movies movies = await db.Movies.FindAsync(id);
+            if (movies == null)
+            {
+                return HttpNotFound();
+            }
+            return View(movies);
+        }
+
+        // POST: Movies/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Movies movies = await db.Movies.FindAsync(id);
+            db.Movies.Remove(movies);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
